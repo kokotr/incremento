67947da5a14422214fdf121d56ed296de375bd69
kokotr <AntonioPerezap077@gmail.com>
2021-11-30 09:33:17 -0600

    esto es una practica de html

---
+++ incremento.js
@@ -0,0 +1,15 @@
+
+
+
+
+function suma(numero1,numero2){
+
+
+var numero1=parseInt(document.getElementById("numero1").value);
+
+var numero2=parseInt(document.getElementById("numero2").value);
+
+
+alert("El resultado de la suma es :"+(numero1+numero2));
+
+}
